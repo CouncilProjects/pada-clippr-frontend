5994031602691c915e66f993188a106076eddb01
CouncilProjects <amorianosdimitris@gmail.com>
2025-10-21 20:37:52 +0300

    Added a sstarting theme. And a lightswitch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import LightSwitch from '$lib/components/LightSwitch.svelte';
 	let { children } = $props();
+	 import { CalendarIcon, CircleUserIcon, MenuIcon, SearchIcon } from '@lucide/svelte';
+  import { AppBar } from '@skeletonlabs/skeleton-svelte';
 </script>
+
+
+<AppBar>
+  <AppBar.Toolbar class="grid-cols-[auto_1fr_auto]">
+    <AppBar.Lead>
+      <button type="button" class="btn-icon btn-icon-lg hover:preset-tonal"><MenuIcon /></button>
+    </AppBar.Lead>
+    <AppBar.Headline>
+      <p class="text-3xl">Clippr</p>
+    </AppBar.Headline>
+    <AppBar.Trail>
+		<LightSwitch></LightSwitch>
+      <button type="button" class="btn-icon hover:preset-tonal"><CircleUserIcon class="size-6" /></button>
+    </AppBar.Trail>
+  </AppBar.Toolbar>
+</AppBar>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h1 class="h1 text-center text-primary-500 m-2">Under construction</h1>
