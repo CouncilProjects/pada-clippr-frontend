d741ee7ffe5a06b5a02e5dbfaabdc3e9cd09ad37
CouncilProjects <amorianosdimitris@gmail.com>
2025-11-16 23:08:28 +0200

    Added verification page for the admins

---
+++ src/routes/verifications/+page.ts
@@ -0,0 +1,16 @@
+import apiCaller from '$lib/axiosConfig';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+    //make api calls when BE is ready 
+
+    try {
+        const {data} = await apiCaller.get("/user/unverified");
+        return {accounts:data}
+    } catch (error) {
+        return {accounts:[]};
+    }
+
+    
+
+}
