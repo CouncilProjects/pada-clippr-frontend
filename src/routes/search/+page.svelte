28b9482e9571ab599c124d3f7f42beb8dbb1f55a
Martin Andronikos <martin.andronikos@gmail.com>
2025-11-23 16:14:18 +0200

    Holy Pagination

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -2,6 +2,8 @@
     import { page } from '$app/state'
     import apiCaller from "$lib/axiosConfig"
     import { MessageSquareDiff } from '@lucide/svelte';
+    import { ArrowLeftIcon, ArrowRightIcon } from '@lucide/svelte';
+    import { Pagination } from '@skeletonlabs/skeleton-svelte';
 
     interface Item {
         title: string
@@ -10,32 +12,78 @@
     }
 
     let searchResults =  $state<Item[]>([])
+    let pageNumber = $state(1)
+    let pageSize = $state(10)
+    let totalItems = $state(0)
 
     async function doSearch() {
         const query = page.url.searchParams.get("q");
         if(query === null) return;
 
         const result = await apiCaller.get("/item/", { params: {
-            q: encodeURIComponent(query)
+            q: encodeURIComponent(query),
+            amount: pageSize,
+            page: pageNumber
         }})
 
         searchResults = result.data.results
-        console.log(searchResults)
+        totalItems = result.data.count
     }
 
      $effect(() => { doSearch() })
 </script>
 
 <div class="overflow-x-auto py-5">
-    <table class="w-full border-collapse"><tbody class="space-y-2">
-        {#each searchResults as item}
-            <tr class="bg-surface-100 dark:bg-surface-800 rounded-lg shadow p-4 flex justify-between items-center">
-                <td>{item.title}</td>
-                <td>€{item.price}</td>
-                <td> {#if item.negotiable}
-                    <MessageSquareDiff />
-                {/if} </td>
+    <div class="flex justify-between items-center gap-4 w-full">
+        <label class="label">
+            <span class="sr-only">Page Size</span>
+            <select class="select w-fit" value={String(pageSize)} onchange={(e) => (pageSize = Number(e.currentTarget.value))}>
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+            </select>
+        </label>
+        <Pagination count={totalItems} {pageSize} page={pageNumber} onPageChange={(event) => (pageNumber = event.page)}>
+            <Pagination.PrevTrigger>
+                <ArrowLeftIcon class="size-4" />
+            </Pagination.PrevTrigger>
+            <Pagination.Context>
+                {#snippet children(pagination)}
+                    {#each pagination().pages as page, index (page)}
+                        {#if page.type === 'page'}
+                            <Pagination.Item {...page}>
+                                {page.value}
+                            </Pagination.Item>
+                        {:else}
+                            <Pagination.Ellipsis {index}>&#8230;</Pagination.Ellipsis>
+                        {/if}
+                    {/each}
+                {/snippet}
+            </Pagination.Context>
+            <Pagination.NextTrigger>
+                <ArrowRightIcon class="size-4" />
+            </Pagination.NextTrigger>
+        </Pagination>
+    </div>
+    <table class="table w-full border-collapse">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Negotiable</th>
             </tr>
-        {/each}
-    </tbody></table>
+        </thead>
+        <tbody class="space-y-2">
+            {#each searchResults as item}
+                <tr>
+                    <td>{item.title}</td>
+                    <td>€{item.price}</td>
+                    <td> {#if item.negotiable}
+                        <MessageSquareDiff />
+                    {/if} </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
 </div>
