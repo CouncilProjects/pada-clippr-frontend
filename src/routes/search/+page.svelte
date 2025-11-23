b47db99b2a92e75d5a9a1c5abec44011153e4e0a
Martin Andronikos <martin.andronikos@gmail.com>
2025-11-23 15:33:50 +0200

    Card-style Table for Search Results

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import { page } from '$app/state'
     import apiCaller from "$lib/axiosConfig"
+    import { MessageSquareDiff } from '@lucide/svelte';
 
     interface Item {
         title: string
+        price: number
+        negotiable: boolean
     }
 
     let searchResults =  $state<Item[]>([])
@@ -17,13 +20,22 @@
         }})
 
         searchResults = result.data.results
+        console.log(searchResults)
     }
 
      $effect(() => { doSearch() })
 </script>
 
-<div>
-    {#each searchResults as item}
-        <div>{item.title}</div>
-    {/each}
+<div class="overflow-x-auto py-5">
+    <table class="w-full border-collapse"><tbody class="space-y-2">
+        {#each searchResults as item}
+            <tr class="bg-surface-100 dark:bg-surface-800 rounded-lg shadow p-4 flex justify-between items-center">
+                <td>{item.title}</td>
+                <td>€{item.price}</td>
+                <td> {#if item.negotiable}
+                    <MessageSquareDiff />
+                {/if} </td>
+            </tr>
+        {/each}
+    </tbody></table>
 </div>
